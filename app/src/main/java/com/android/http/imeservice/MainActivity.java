659de8389599827a2a983d581ffd3a122d1c4c9b
muxi01 <mrsha1195@163.com>
2019-12-13 11:47:06 +0800

    first commit

---
+++ app/src/main/java/com/android/http/imeservice/MainActivity.java
@@ -0,0 +1,27 @@
+package com.android.http.imeservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnSet =(Button) findViewById(R.id.btnSystemSetting);
+        btnSet.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+}
